5467c6eb8dce0236401fd2cdd94e0705475baa0a
Natsu <paz171092@gmail.com>
2025-09-10 00:31:55 -0300

    renderizado de tajetas en HTML

--- render/renderer.js
+++ render/renderer.js
@@ -1,17 +1,31 @@
 const { ipcRenderer } = require("electron");
-
-const { getDatabase, obtenerData } = require('../database/database.js');
+//conexion base de datos
+const { getDatabase, obtenerData } = require("../database/database.js");
 const database = getDatabase();
-
-const turnos = document.getElementById('agenda-cont');
-
+//obtencion elemento padre
+const turnos = document.getElementById("agenda-cont");
+//renderizado de electron hacia pagina principal, insercion nuevos datos
 ipcRenderer.on("send-task", (e, newTask) => {
-  const { cliente, servicio, fecha, descripcion} = newTask;
-  const query = database.prepare('INSERT INTO tareas (fecha, cliente, servicio, descripcion) VALUES ( ? , ? , ? , ?)');
+  const { cliente, servicio, fecha, descripcion } = newTask;
+  const query = database.prepare(
+    "INSERT INTO tareas (fecha, cliente, servicio, descripcion) VALUES ( ? , ? , ? , ?)"
+  );
   query.run(fecha, cliente, servicio, descripcion);
 });
 
 const listaTareas = obtenerData();
-for (let i = 0; i < listaTareas.length; i++){
-    // console.log(listaTareas[i])
+
+listaTareas.sort((a, b) => {
+  return new Date(a.fecha).getTime() - new Date(b.fecha).getTime();
+});
+
+for (let i = 0; i < listaTareas.length; i++) {
+  let cardTemplate = `<div class="card">
+          <h3>${listaTareas[i].cliente}</h3>
+          <p>${listaTareas[i].fecha}</p>
+          <button>X</button>
+          <h4>${listaTareas[i].servicio}</h4>
+          <p>${listaTareas[i].descripcion}</p>
+        </div>`;
+  turnos.innerHTML += cardTemplate;
 }
